8610b9115853296c385c0fb5c41dec24dd8ee2ac
Thor Whalen <thorwhalen1@gmail.com>
2021-06-30 10:32:53 +0200

    protecting from import

--- meshed/makers.py
+++ meshed/makers.py
@@ -1,30 +1,34 @@
 """Makers"""
 
+from contextlib import suppress
 from typing import Mapping, Iterable, TypeVar, Callable
 from itertools import product
 from collections import defaultdict
-from numpy.random import randint, choice
-
-T = TypeVar('T')
 
 
-def random_graph(n_nodes=7):
-    """Get a random graph"""
-    nodes = range(n_nodes)
+T = TypeVar("T")
 
-    def gen():
-        for src in nodes:
-            n_dst = randint(0, n_nodes - 1)
-            dst = choice(n_nodes, n_dst, replace=False)
-            yield src, list(dst)
+with suppress(ModuleNotFoundError, ImportError):
+    from numpy.random import randint, choice
 
-    return dict(gen())
+    def random_graph(n_nodes=7):
+        """Get a random graph"""
+        nodes = range(n_nodes)
+
+        def gen():
+            for src in nodes:
+                n_dst = randint(0, n_nodes - 1)
+                dst = choice(n_nodes, n_dst, replace=False)
+                yield src, list(dst)
+
+        return dict(gen())
 
 
-def edge_reversed_graph(g: Mapping[T, Iterable[T]],
-                        dst_nodes_factory: Callable[[], Iterable[T]] = list,
-                        dst_nodes_append: Callable[[Iterable[T], T], None] = list.append
-                        ) -> Mapping[T, Iterable[T]]:
+def edge_reversed_graph(
+    g: Mapping[T, Iterable[T]],
+    dst_nodes_factory: Callable[[], Iterable[T]] = list,
+    dst_nodes_append: Callable[[Iterable[T], T], None] = list.append,
+) -> Mapping[T, Iterable[T]]:
     """
     >>> g = dict(a='c', b='cd', c='abd', e='')
     >>> assert edge_reversed_graph(g) == {'c': ['a', 'b'], 'd': ['b', 'c'], 'a': ['c'], 'b': ['c'], 'e': []}
