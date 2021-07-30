6029714d331e4e40f31bccc4acabc0f3b1e8a541
Thor Whalen <thorwhalen1@gmail.com>
2021-07-30 16:13:38 +0100

    0.1.3:

---
+++ meshed/__init__.py
@@ -0,0 +1,13 @@
+"""Tools that enable operations on graphs where grapsh are represented by an adjacency Mapping.
+
+"""
+
+from meshed.itools import (
+    edges,
+    nodes,
+)
+
+from meshed.dag import (
+    DAG,
+    FuncNode,
+)
