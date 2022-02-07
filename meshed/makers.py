e14092f8bf1cf9a31f22ecb57ee61966282acfce
Thor Whalen <thorwhalen1@gmail.com>
2022-02-07 12:15:22 -0800

    close: #9 (simple_code_to_digraph)

--- meshed/makers.py
+++ meshed/makers.py
@@ -1,13 +1,63 @@
 """Makers"""
 
 from contextlib import suppress
+from functools import partial
 from typing import Mapping, Iterable, TypeVar, Callable
 from itertools import product
 from collections import defaultdict
 
 
-T = TypeVar('T')
+T = TypeVar("T")
 
+# TODO: This function should really be a DAG where we can choose if we want parsed lines,
+#   digraph dot commands, the graphviz.Digraph object itself etc.
+# TODO: The function below is meant to evolve into a tool that can take python code,
+#  (possibly written with some style constraints) and produce an equivalent DAG.
+#   Here, it just produces a graphviz.Digraph of the code.
+def simple_code_to_digraph(code):
+    """Make a graphviz.Digraph object based on code (string or function's body)"""
+    import re
+    from inspect import getsource
+    from graphviz import Digraph
+
+    def get_code_str(code) -> str:
+        if not isinstance(code, str):
+            return getsource(code)
+        return code
+
+    empty_spaces = re.compile("^\s*$")
+    simple_assignment_p = re.compile(
+        "(?P<output_vars>[^=]+)" "\s*=\s*" "(?P<func>\w+)" "\((?P<input_vars>.*)\)"
+    )
+
+    def get_lines(code_str):
+        for line in code_str.split("\n"):
+            if not empty_spaces.match(line):
+                yield line.strip()
+
+    def groupdict_parser(s, pattern):
+        pattern = re.compile(pattern)
+        m = pattern.search(s)
+        if m:
+            return m.groupdict()
+        return None
+
+    def parsed_lines(
+        code_str, line_parser=partial(groupdict_parser, pattern=simple_assignment_p)
+    ):
+        yield from filter(None, map(line_parser, get_lines(code_str)))
+
+    def parsed_lines_to_dot(parsed_lines):
+        for d in parsed_lines:
+            yield f"{d['func']} [shape=box]"
+            yield f"{d['input_vars']} -> {d['func']} -> {d['output_vars']}"
+
+    code_str = get_code_str(code)
+    dot_lines = parsed_lines_to_dot(parsed_lines(code_str))
+    return Digraph(body=dot_lines)
+
+
+# TODO: Replace using builtin random
 with suppress(ModuleNotFoundError, ImportError):
     from numpy.random import randint, choice
 
