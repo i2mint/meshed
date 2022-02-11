28365e172d1c1a3400fef0f7b1d7a182fee305e4
Thor Whalen <thorwhalen1@gmail.com>
2022-02-10 17:40:18 -0800

    feat: primer in ast parsing to get from assignments to func node factories

--- meshed/makers.py
+++ meshed/makers.py
@@ -7,7 +7,107 @@ from itertools import product
 from collections import defaultdict
 
 
-T = TypeVar('T')
+T = TypeVar("T")
+
+# Some restrictions exist and need to be clarified or removed (i.e. more cases handled)
+# For example,
+# * tuple assignment not handled (x, y = func(...))
+# * can't reuse a variable (would lead to same node)
+# * x = y (or x, y = tup) not handled (but could easily by binding)
+# We don't need these cases to be handled, only x = func(...) forms lead to Turing (I
+# think...):
+# Further other cases are not handled, but we don't want to handle ALL of python
+# -- just a sufficiently expressive subset.
+
+import ast
+import inspect
+from typing import Tuple, Callable
+
+
+def attr_dict(obj):
+    return {a: getattr(obj, a) for a in dir(obj) if not a.startswith("_")}
+
+
+def is_from_ast_module(o):
+    return getattr(type(o), "__module__", "").startswith("_ast")
+
+
+def _ast_info_str(x):
+    return f"lineno={x.lineno}"
+
+
+# Note: generalize? glom?
+def parse_assignment(body: ast.Assign) -> Tuple:
+    # TODO: Make this validation better (at least more help in raised error)
+    # TODO: extract validating code out as validation functions?
+    info = _ast_info_str(body)
+    if not isinstance(body, ast.Assign):
+        raise ValueError(f"All commands should be assignments, this one wasn't: {info}")
+
+    target = body.targets
+    assert len(target) == 1, f"Only one target allowed: {info}"
+    target = target[0]
+    assert isinstance(
+        target, (ast.Name, ast.Tuple)
+    ), f"Should be a ast.Name or ast.Tuple: {info}"
+
+    value = body.value
+    assert isinstance(value, ast.Call), f"Only one target allowed: {info}"
+
+    return target, value
+
+
+def parsed_to_node_kwargs(target_value) -> dict:
+    # Note: ast.Tuple has names in 'elts' attribute,
+    # and could be handled, but would need to lead to multiple nodes
+    target, value = target_value
+    assert isinstance(target, ast.Name), f"Should be a ast.Name: {target}"
+    args = value.args
+    # TODO: we're binding arg name to arg name here, but if the actual func
+    # doesn't use that arg name, the FuncNode won't work.
+    # Need to find a solution here.
+    # For example, late binding, only when the function is injected (therefore the
+    # signature known?)
+    # In that case, better just put args and keywords in here, and make the bind arg
+    # once the func is known
+    bind_from_args = {k.id: k.id for k in args}
+    kwargs = {x.arg: x.value.id for x in value.keywords}
+    return dict(name=value.func.id, out=target.id, bind=dict(bind_from_args, **kwargs))
+
+
+from meshed import FuncNode
+
+FuncNodeFactory = Callable[[Callable], FuncNode]
+
+
+def node_kwargs_to_func_node_factory(node_kwargs) -> FuncNodeFactory:
+    return partial(FuncNode, **node_kwargs)
+
+
+def parse_assignment_steps(src):
+    if callable(src):
+        src = inspect.getsource(src)
+    root = ast.parse(src)
+    assert len(root.body) == 1
+    func_body = root.body[0]
+    # TODO, work with func_body.args to get info on interface (name, args, kwargs,
+    #  return etc.)
+    #     return func_body
+    for body in func_body.body:
+        yield parse_assignment(body)
+
+
+from i2 import Pipe
+
+iterize = lambda func: partial(map, func)
+
+targval_to_func_node_factory = Pipe(
+    parsed_to_node_kwargs, node_kwargs_to_func_node_factory
+)
+src_to_func_node_factory = Pipe(
+    parse_assignment_steps, iterize(targval_to_func_node_factory)
+)
+
 
 # TODO: This function should really be a DAG where we can choose if we want parsed lines,
 #   digraph dot commands, the graphviz.Digraph object itself etc.
@@ -25,13 +125,13 @@ def simple_code_to_digraph(code):
             return getsource(code)
         return code
 
-    empty_spaces = re.compile('^\s*$')
+    empty_spaces = re.compile("^\s*$")
     simple_assignment_p = re.compile(
-        '(?P<output_vars>[^=]+)' '\s*=\s*' '(?P<func>\w+)' '\((?P<input_vars>.*)\)'
+        "(?P<output_vars>[^=]+)" "\s*=\s*" "(?P<func>\w+)" "\((?P<input_vars>.*)\)"
     )
 
     def get_lines(code_str):
-        for line in code_str.split('\n'):
+        for line in code_str.split("\n"):
             if not empty_spaces.match(line):
                 yield line.strip()
 
