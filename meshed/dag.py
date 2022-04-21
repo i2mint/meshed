78fa649bfd4348015a604afd5d45fe60949a2e52
sylvainbonnot <sppbonnot@gmail.com>
2022-04-21 15:56:32 +0200

    order subgraph nodes

--- meshed/dag.py
+++ meshed/dag.py
@@ -169,23 +169,30 @@ from meshed.itools import (
 )
 
 
+def order_subset_from_list(items, sublist):
+    assert set(sublist).issubset(set(items)), f"{sublist} is not contained in {items}"
+    d = {k: v for v, k in enumerate(items)}
+
+    return sorted(sublist, key=lambda x: d[x])
+
+
 def find_first_free_name(prefix, exclude_names=(), start_at=2):
     if prefix not in exclude_names:
         return prefix
     else:
         i = start_at
         while True:
-            name = f'{prefix}__{i}'
+            name = f"{prefix}__{i}"
             if name not in exclude_names:
                 return name
             i += 1
 
 
 def mk_func_name(func, exclude_names=()):
-    name = getattr(func, '__name__', '')
-    if name == '<lambda>':
+    name = getattr(func, "__name__", "")
+    if name == "<lambda>":
         name = lambda_name()  # make a lambda name that is a unique identifier
-    elif name == '':
+    elif name == "":
         if isinstance(func, partial):
             return mk_func_name(func.func, exclude_names)
         else:
@@ -203,7 +210,7 @@ def arg_names(func, func_name, exclude_names=()):
                 yield name
             else:
                 found_name = find_first_free_name(
-                    f'{func_name}__{name}', _exclude_names
+                    f"{func_name}__{name}", _exclude_names
                 )
                 yield found_name
                 _exclude_names = _exclude_names + (found_name,)
@@ -299,7 +306,7 @@ def _complete_dict_with_iterable_of_required_keys(
 def _inverse_dict_asserting_losslessness(d: dict):
     inv_d = {v: k for k, v in d.items()}
     assert len(inv_d) == len(d), (
-        f"can't invert: You have some duplicate values in this dict: " f'{d}'
+        f"can't invert: You have some duplicate values in this dict: " f"{d}"
     )
     return inv_d
 
@@ -382,7 +389,7 @@ def conservative_parameter_merge(
     But if they're not the same, we need to provide control on which to ignore.
 
     """
-    suggestion_on_error = '''To resolve this you have several choices:
+    suggestion_on_error = """To resolve this you have several choices:
     
     - Change the properties of the param (kind, default, annotation) to be those you 
       want. For example, you can use ``i2.Sig.ch_param_attrs`` 
@@ -395,7 +402,7 @@ def conservative_parameter_merge(
       
     See https://github.com/i2mint/meshed/issues/7 (description and comments) for more
     info.
-    '''
+    """
     first_param, *_ = params
     if not all(p.name == first_param.name for p in params):
         raise ValidationError(
@@ -414,21 +421,21 @@ def conservative_parameter_merge(
     ):
         raise ValidationError(
             f"Some params didn't have the same annotation: "
-            f'{params}\n{suggestion_on_error}'
+            f"{params}\n{suggestion_on_error}"
         )
     return first_param
 
 
 def modified_func_node(func_node, **modifications) -> FuncNode:
-    modifiable_attrs = {'func', 'name', 'bind', 'out'}
+    modifiable_attrs = {"func", "name", "bind", "out"}
     assert not modifications.keys().isdisjoint(
         modifiable_attrs
     ), f"Can only modify these: {', '.join(modifiable_attrs)}"
     original_func_node_kwargs = {
-        'func': func_node.func,
-        'name': func_node.name,
-        'bind': func_node.bind,
-        'out': func_node.out,
+        "func": func_node.func,
+        "name": func_node.name,
+        "bind": func_node.bind,
+        "out": func_node.out,
     }
     return FuncNode(**dict(original_func_node_kwargs, **modifications))
 
@@ -462,7 +469,7 @@ DagOutput = Any
 
 
 def _name_attr_or_x(x):
-    return getattr(x, 'name', x)
+    return getattr(x, "name", x)
 
 
 # TODO: caching last scope isn't really the DAG's direct concern -- it's a debugging
@@ -713,8 +720,11 @@ class DAG:
         subgraph_nodes = _descendants.intersection(_ancestors)
         # TODO: When clone ready, use to do `constructor = type(self)` instead of DAG
         # constructor = type(self)  # instead of DAG
+        initial_nodes = self.func_nodes
+        ordered_subgraph_nodes = order_subset_from_list(initial_nodes, subgraph_nodes)
+
         return DAG(
-            func_nodes=subgraph_nodes,
+            func_nodes=ordered_subgraph_nodes,
             cache_last_scope=self.cache_last_scope,
             parameter_merge=self.parameter_merge,
         )
@@ -788,7 +798,7 @@ class DAG:
         return new_dag
 
     def process_item(self, item):
-        assert isinstance(item, slice), f'must be a slice, was: {item}'
+        assert isinstance(item, slice), f"must be a slice, was: {item}"
 
         input_names, outs = item.start, item.stop
 
@@ -804,7 +814,7 @@ class DAG:
             elif isinstance(obj, Iterable):
                 return list(map(self.get_node_matching, obj))
             else:
-                raise ValidationError(f'Unrecognized variables specification: {obj}')
+                raise ValidationError(f"Unrecognized variables specification: {obj}")
 
         # assert len(item) == 2, f"Only items of size 1 or 2 are supported"
         input_names, outs = map(ensure_variable_list, [input_names, outs])
@@ -817,7 +827,7 @@ class DAG:
             return self.func_node_for_name(pattern)
         elif isinstance(pattern, Callable):
             return self.func_node_for_func(pattern)
-        raise NotFound(f'No matching node: {pattern}')
+        raise NotFound(f"No matching node: {pattern}")
 
     def func_node_for_name(self, name):
         return _find_unique_element(
@@ -915,7 +925,7 @@ class DAG:
     # ------------ display --------------------------------------------------------------
 
     def synopsis_string(self):
-        return '\n'.join(func_node.synopsis_string() for func_node in self.func_nodes)
+        return "\n".join(func_node.synopsis_string() for func_node in self.func_nodes)
 
     # TODO: Give more control (merge with lined)
     def dot_digraph_body(self, start_lines=()):
@@ -939,7 +949,7 @@ class DAG:
         """Get an ascii art string that represents the pipeline"""
         from meshed.util import dot_to_ascii
 
-        return dot_to_ascii('\n'.join(self.dot_digraph_body(*args, **kwargs)))
+        return dot_to_ascii("\n".join(self.dot_digraph_body(*args, **kwargs)))
 
     @wraps(dot_digraph_body)
     def dot_digraph(self, *args, **kwargs):
@@ -947,8 +957,8 @@ class DAG:
             import graphviz
         except (ModuleNotFoundError, ImportError) as e:
             raise ModuleNotFoundError(
-                f'{e}\nYou may not have graphviz installed. '
-                f'See https://pypi.org/project/graphviz/.'
+                f"{e}\nYou may not have graphviz installed. "
+                f"See https://pypi.org/project/graphviz/."
             )
         # Note: Since graphviz 0.18, need to have a newline in body lines!
         body = list(map(_add_new_line_if_none, self.dot_digraph_body(*args, **kwargs)))
@@ -958,22 +968,22 @@ class DAG:
 # These are the defaults used in lined.
 # TODO: Merge some of the functionalities around graph displays in lined and meshed
 dflt_configs = dict(
-    fnode_shape='box',
-    vnode_shape='none',
+    fnode_shape="box",
+    vnode_shape="none",
     display_all_arguments=True,
-    edge_kind='to_args_on_edge',
+    edge_kind="to_args_on_edge",
     input_node=True,
-    output_node='output',
+    output_node="output",
 )
 
 
-def param_to_dot_definition(p: Parameter, shape=dflt_configs['vnode_shape']):
+def param_to_dot_definition(p: Parameter, shape=dflt_configs["vnode_shape"]):
     if p.default is not empty:
-        name = p.name + '='
+        name = p.name + "="
     elif p.kind == p.VAR_POSITIONAL:
-        name = '*' + p.name
+        name = "*" + p.name
     elif p.kind == p.VAR_KEYWORD:
-        name = '**' + p.name
+        name = "**" + p.name
     else:
         name = p.name
     yield f'{p.name} [label="{name}" shape="{shape}"]'
@@ -990,30 +1000,32 @@ def dot_lines_of_func_parameters(
     func_id: str,
     *,
     func_label: str = None,
-    output_shape: str = dflt_configs['vnode_shape'],
-    func_shape: str = dflt_configs['fnode_shape'],
+    output_shape: str = dflt_configs["vnode_shape"],
+    func_shape: str = dflt_configs["fnode_shape"],
 ) -> Iterable[str]:
     assert func_id != out, (
-        f"Your func and output name shouldn't be the " f'same: {out=} {func_id=}'
+        f"Your func and output name shouldn't be the " f"same: {out=} {func_id=}"
     )
     func_label = func_label or func_id
     yield f'{out} [label="{out}" shape="{output_shape}"]'
     yield f'{func_id} [label="{func_label}" shape="{func_shape}"]'
-    yield f'{func_id} -> {out}'
+    yield f"{func_id} -> {out}"
     # args -> func
     for p in parameters:
         yield from param_to_dot_definition(p)
     for p in parameters:
-        yield f'{p.name} -> {func_id}'
+        yield f"{p.name} -> {func_id}"
 
 
 def _parameters_and_names_from_sig(
-    sig: Sig, out=None, func_name=None,
+    sig: Sig,
+    out=None,
+    func_name=None,
 ):
     func_name = func_name or sig.name
     out = out or sig.name
     if func_name == out:
-        func_name = '_' + func_name
+        func_name = "_" + func_name
     assert isinstance(func_name, str) and isinstance(out, str)
     return sig.parameters, out, func_name
 
@@ -1111,9 +1123,9 @@ def dot_lines_of_func_node(func_node: FuncNode):
 
     out = func_node.out
     func_id = func_node.name
-    func_label = getattr(func_node, 'func_label', func_id)
+    func_label = getattr(func_node, "func_label", func_id)
     if out == func_id:  # though forbidden in default FuncNode validation
-        func_id = '_' + func_id
+        func_id = "_" + func_id
 
     # Get the Parameter objects for sig, with names changed to bind ones
     params = func_node.sig.ch_names(**func_node.bind).params
@@ -1127,8 +1139,8 @@ def _add_new_line_if_none(s: str):
     """Since graphviz 0.18, need to have a newline in body lines.
     This util is there to address that, adding newlines to body lines
     when missing."""
-    if s and s[-1] != '\n':
-        return s + '\n'
+    if s and s[-1] != "\n":
+        return s + "\n"
     return s
 
 
@@ -1146,7 +1158,10 @@ with suppress(ModuleNotFoundError, ImportError):
             needs = arg_names(func, _func_name, exclude_names)
             exclude_names = exclude_names + tuple(needs)
             yield operation(
-                func, name=_func_name, needs=needs, provides=_func_name,
+                func,
+                name=_func_name,
+                needs=needs,
+                provides=_func_name,
             )
 
     def funcs_to_operators(*funcs, exclude_names=()) -> Operation:
