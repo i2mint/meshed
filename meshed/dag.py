62ab352db7618777980bb62cf26cb545ca1055b8
GitHub CI Runner <thorwhalen1@gmail.com>
2022-04-29 12:48:28 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- meshed/dag.py
+++ meshed/dag.py
@@ -170,7 +170,7 @@ from meshed.itools import (
 
 
 def order_subset_from_list(items, sublist):
-    assert set(sublist).issubset(set(items)), f"{sublist} is not contained in {items}"
+    assert set(sublist).issubset(set(items)), f'{sublist} is not contained in {items}'
     d = {k: v for v, k in enumerate(items)}
 
     return sorted(sublist, key=lambda x: d[x])
@@ -182,7 +182,7 @@ def find_first_free_name(prefix, exclude_names=(), start_at=2):
     else:
         i = start_at
         while True:
-            name = f"{prefix}__{i}"
+            name = f'{prefix}__{i}'
             if name not in exclude_names:
                 return name
             i += 1
@@ -194,16 +194,16 @@ def mk_mock_funcnode(arg, out):
         pass
 
     # name = "_mock_" + str(arg) + "_" + str(out)  # f-string
-    name = f"_mock_{str(arg)}_{str(out)}"  # f-string
+    name = f'_mock_{str(arg)}_{str(out)}'  # f-string
 
     return FuncNode(func=func, out=out, name=name)
 
 
 def mk_func_name(func, exclude_names=()):
-    name = getattr(func, "__name__", "")
-    if name == "<lambda>":
+    name = getattr(func, '__name__', '')
+    if name == '<lambda>':
         name = lambda_name()  # make a lambda name that is a unique identifier
-    elif name == "":
+    elif name == '':
         if isinstance(func, partial):
             return mk_func_name(func.func, exclude_names)
         else:
@@ -221,7 +221,7 @@ def arg_names(func, func_name, exclude_names=()):
                 yield name
             else:
                 found_name = find_first_free_name(
-                    f"{func_name}__{name}", _exclude_names
+                    f'{func_name}__{name}', _exclude_names
                 )
                 yield found_name
                 _exclude_names = _exclude_names + (found_name,)
@@ -317,7 +317,7 @@ def _complete_dict_with_iterable_of_required_keys(
 def _inverse_dict_asserting_losslessness(d: dict):
     inv_d = {v: k for k, v in d.items()}
     assert len(inv_d) == len(d), (
-        f"can't invert: You have some duplicate values in this dict: " f"{d}"
+        f"can't invert: You have some duplicate values in this dict: " f'{d}'
     )
     return inv_d
 
@@ -400,7 +400,7 @@ def conservative_parameter_merge(
     But if they're not the same, we need to provide control on which to ignore.
 
     """
-    suggestion_on_error = """To resolve this you have several choices:
+    suggestion_on_error = '''To resolve this you have several choices:
     
     - Change the properties of the param (kind, default, annotation) to be those you 
       want. For example, you can use ``i2.Sig.ch_param_attrs`` 
@@ -413,7 +413,7 @@ def conservative_parameter_merge(
       
     See https://github.com/i2mint/meshed/issues/7 (description and comments) for more
     info.
-    """
+    '''
     first_param, *_ = params
     if not all(p.name == first_param.name for p in params):
         raise ValidationError(
@@ -432,21 +432,21 @@ def conservative_parameter_merge(
     ):
         raise ValidationError(
             f"Some params didn't have the same annotation: "
-            f"{params}\n{suggestion_on_error}"
+            f'{params}\n{suggestion_on_error}'
         )
     return first_param
 
 
 def modified_func_node(func_node, **modifications) -> FuncNode:
-    modifiable_attrs = {"func", "name", "bind", "out"}
+    modifiable_attrs = {'func', 'name', 'bind', 'out'}
     assert not modifications.keys().isdisjoint(
         modifiable_attrs
     ), f"Can only modify these: {', '.join(modifiable_attrs)}"
     original_func_node_kwargs = {
-        "func": func_node.func,
-        "name": func_node.name,
-        "bind": func_node.bind,
-        "out": func_node.out,
+        'func': func_node.func,
+        'name': func_node.name,
+        'bind': func_node.bind,
+        'out': func_node.out,
     }
     return FuncNode(**dict(original_func_node_kwargs, **modifications))
 
@@ -480,7 +480,7 @@ DagOutput = Any
 
 
 def _name_attr_or_x(x):
-    return getattr(x, "name", x)
+    return getattr(x, 'name', x)
 
 
 # TODO: caching last scope isn't really the DAG's direct concern -- it's a debugging
@@ -809,7 +809,7 @@ class DAG:
         return new_dag
 
     def process_item(self, item):
-        assert isinstance(item, slice), f"must be a slice, was: {item}"
+        assert isinstance(item, slice), f'must be a slice, was: {item}'
 
         input_names, outs = item.start, item.stop
 
@@ -825,7 +825,7 @@ class DAG:
             elif isinstance(obj, Iterable):
                 return list(map(self.get_node_matching, obj))
             else:
-                raise ValidationError(f"Unrecognized variables specification: {obj}")
+                raise ValidationError(f'Unrecognized variables specification: {obj}')
 
         # assert len(item) == 2, f"Only items of size 1 or 2 are supported"
         input_names, outs = map(ensure_variable_list, [input_names, outs])
@@ -838,7 +838,7 @@ class DAG:
             return self.func_node_for_name(pattern)
         elif isinstance(pattern, Callable):
             return self.func_node_for_func(pattern)
-        raise NotFound(f"No matching node: {pattern}")
+        raise NotFound(f'No matching node: {pattern}')
 
     def func_node_for_name(self, name):
         return _find_unique_element(
@@ -936,7 +936,7 @@ class DAG:
     # ------------ display --------------------------------------------------------------
 
     def synopsis_string(self):
-        return "\n".join(func_node.synopsis_string() for func_node in self.func_nodes)
+        return '\n'.join(func_node.synopsis_string() for func_node in self.func_nodes)
 
     # TODO: Give more control (merge with lined)
     def dot_digraph_body(self, start_lines=()):
@@ -960,7 +960,7 @@ class DAG:
         """Get an ascii art string that represents the pipeline"""
         from meshed.util import dot_to_ascii
 
-        return dot_to_ascii("\n".join(self.dot_digraph_body(*args, **kwargs)))
+        return dot_to_ascii('\n'.join(self.dot_digraph_body(*args, **kwargs)))
 
     @wraps(dot_digraph_body)
     def dot_digraph(self, *args, **kwargs):
@@ -968,8 +968,8 @@ class DAG:
             import graphviz
         except (ModuleNotFoundError, ImportError) as e:
             raise ModuleNotFoundError(
-                f"{e}\nYou may not have graphviz installed. "
-                f"See https://pypi.org/project/graphviz/."
+                f'{e}\nYou may not have graphviz installed. '
+                f'See https://pypi.org/project/graphviz/.'
             )
         # Note: Since graphviz 0.18, need to have a newline in body lines!
         body = list(map(_add_new_line_if_none, self.dot_digraph_body(*args, **kwargs)))
@@ -979,22 +979,22 @@ class DAG:
 # These are the defaults used in lined.
 # TODO: Merge some of the functionalities around graph displays in lined and meshed
 dflt_configs = dict(
-    fnode_shape="box",
-    vnode_shape="none",
+    fnode_shape='box',
+    vnode_shape='none',
     display_all_arguments=True,
-    edge_kind="to_args_on_edge",
+    edge_kind='to_args_on_edge',
     input_node=True,
-    output_node="output",
+    output_node='output',
 )
 
 
-def param_to_dot_definition(p: Parameter, shape=dflt_configs["vnode_shape"]):
+def param_to_dot_definition(p: Parameter, shape=dflt_configs['vnode_shape']):
     if p.default is not empty:
-        name = p.name + "="
+        name = p.name + '='
     elif p.kind == p.VAR_POSITIONAL:
-        name = "*" + p.name
+        name = '*' + p.name
     elif p.kind == p.VAR_KEYWORD:
-        name = "**" + p.name
+        name = '**' + p.name
     else:
         name = p.name
     yield f'{p.name} [label="{name}" shape="{shape}"]'
@@ -1011,32 +1011,30 @@ def dot_lines_of_func_parameters(
     func_id: str,
     *,
     func_label: str = None,
-    output_shape: str = dflt_configs["vnode_shape"],
-    func_shape: str = dflt_configs["fnode_shape"],
+    output_shape: str = dflt_configs['vnode_shape'],
+    func_shape: str = dflt_configs['fnode_shape'],
 ) -> Iterable[str]:
     assert func_id != out, (
-        f"Your func and output name shouldn't be the " f"same: {out=} {func_id=}"
+        f"Your func and output name shouldn't be the " f'same: {out=} {func_id=}'
     )
     func_label = func_label or func_id
     yield f'{out} [label="{out}" shape="{output_shape}"]'
     yield f'{func_id} [label="{func_label}" shape="{func_shape}"]'
-    yield f"{func_id} -> {out}"
+    yield f'{func_id} -> {out}'
     # args -> func
     for p in parameters:
         yield from param_to_dot_definition(p)
     for p in parameters:
-        yield f"{p.name} -> {func_id}"
+        yield f'{p.name} -> {func_id}'
 
 
 def _parameters_and_names_from_sig(
-    sig: Sig,
-    out=None,
-    func_name=None,
+    sig: Sig, out=None, func_name=None,
 ):
     func_name = func_name or sig.name
     out = out or sig.name
     if func_name == out:
-        func_name = "_" + func_name
+        func_name = '_' + func_name
     assert isinstance(func_name, str) and isinstance(out, str)
     return sig.parameters, out, func_name
 
@@ -1134,9 +1132,9 @@ def dot_lines_of_func_node(func_node: FuncNode):
 
     out = func_node.out
     func_id = func_node.name
-    func_label = getattr(func_node, "func_label", func_id)
+    func_label = getattr(func_node, 'func_label', func_id)
     if out == func_id:  # though forbidden in default FuncNode validation
-        func_id = "_" + func_id
+        func_id = '_' + func_id
 
     # Get the Parameter objects for sig, with names changed to bind ones
     params = func_node.sig.ch_names(**func_node.bind).params
@@ -1150,8 +1148,8 @@ def _add_new_line_if_none(s: str):
     """Since graphviz 0.18, need to have a newline in body lines.
     This util is there to address that, adding newlines to body lines
     when missing."""
-    if s and s[-1] != "\n":
-        return s + "\n"
+    if s and s[-1] != '\n':
+        return s + '\n'
     return s
 
 
@@ -1169,10 +1167,7 @@ with suppress(ModuleNotFoundError, ImportError):
             needs = arg_names(func, _func_name, exclude_names)
             exclude_names = exclude_names + tuple(needs)
             yield operation(
-                func,
-                name=_func_name,
-                needs=needs,
-                provides=_func_name,
+                func, name=_func_name, needs=needs, provides=_func_name,
             )
 
     def funcs_to_operators(*funcs, exclude_names=()) -> Operation:
@@ -1197,7 +1192,7 @@ def reorder_on_constraints(funcnodes, outs):
     funcnodes += extra_nodes
     graph = _func_nodes_to_graph_dict(funcnodes)
     nodes = topological_sort(graph)
-    print("after ordering:", nodes)
+    print('after ordering:', nodes)
     ordered_nodes = [node for node in nodes if node not in extra_nodes]
     func_nodes, var_nodes = _separate_func_nodes_and_var_nodes(ordered_nodes)
 
--- meshed/util.py
+++ meshed/util.py
@@ -5,13 +5,13 @@ from typing import Callable, Any, Union, Iterator, Optional, Iterable
 from i2 import Sig, name_of_obj
 
 
-def extra_wraps(func, name=None, doc_prefix=""):
+def extra_wraps(func, name=None, doc_prefix=''):
     func.__name__ = name or func_name(func)
-    func.__doc__ = doc_prefix + getattr(func, "__name__", "")
+    func.__doc__ = doc_prefix + getattr(func, '__name__', '')
     return func
 
 
-def mywraps(func, name=None, doc_prefix=""):
+def mywraps(func, name=None, doc_prefix=''):
     def wrapper(wrapped):
         return extra_wraps(wraps(func)(wrapped), name=name, doc_prefix=doc_prefix)
 
@@ -77,7 +77,7 @@ def iterize(func, name=None):
     """
     # TODO: See if partialx can be used instead
     wrapper = mywraps(
-        func, name=name, doc_prefix=f"generator version of {func_name(func)}:\n"
+        func, name=name, doc_prefix=f'generator version of {func_name(func)}:\n'
     )
     return wrapper(partial(map, func))
 
@@ -246,13 +246,13 @@ class ConditionalIterize:
             return self.func(*args, **kwargs)
 
     def __repr__(self):
-        return f"<ConditionalIterize {name_of_obj(self)}{Sig(self)}>"
+        return f'<ConditionalIterize {name_of_obj(self)}{Sig(self)}>'
 
     def _new_sig(self):
         if len(self.sig.names) == 0:
             raise TypeError(
-                f"You can only apply conditional iterization on functions that have "
-                f"at least one input. This one had none: {self.func}"
+                f'You can only apply conditional iterization on functions that have '
+                f'at least one input. This one had none: {self.func}'
             )
         first_param = self.sig.names[0]
         new_sig = self.sig  # same sig by default
@@ -284,7 +284,7 @@ class ModuleNotFoundIgnore:
         return True
 
 
-def incremental_str_maker(str_format="{:03.f}"):
+def incremental_str_maker(str_format='{:03.f}'):
     """Make a function that will produce a (incrementally) new string at every call."""
     i = 0
 
@@ -296,8 +296,8 @@ def incremental_str_maker(str_format="{:03.f}"):
     return mk_next_str
 
 
-lambda_name = incremental_str_maker(str_format="lambda_{:03.0f}")
-unnameable_func_name = incremental_str_maker(str_format="unnameable_func_{:03.0f}")
+lambda_name = incremental_str_maker(str_format='lambda_{:03.0f}')
+unnameable_func_name = incremental_str_maker(str_format='unnameable_func_{:03.0f}')
 
 FunctionNamer = Callable[[Callable], str]
 
@@ -309,7 +309,7 @@ def func_name(func) -> str:
     To make one, it calls unamed_func_name which produces incremental names to reduce the chances of clashing"""
     try:
         name = func.__name__
-        if name == "<lambda>":
+        if name == '<lambda>':
             return lambda_name()
         return name
     except AttributeError:
@@ -331,11 +331,11 @@ def args_funcnames(
     for func in funcs:
         sig = signature(func)
         for param in sig.parameters.values():
-            arg_name = ""  # initialize
+            arg_name = ''  # initialize
             if param.kind == Parameter.VAR_POSITIONAL:
-                arg_name += "*"
+                arg_name += '*'
             elif param.kind == Parameter.VAR_KEYWORD:
-                arg_name += "**"
+                arg_name += '**'
             arg_name += param.name  # append name of param
             yield arg_name, name_of_func(func)
 
@@ -345,7 +345,7 @@ def funcs_to_digraph(funcs, graph=None):
 
     graph = graph or Digraph()
     graph.edges(list(args_funcnames(funcs)))
-    graph.body.extend([", ".join(func.__name__ for func in funcs) + " [shape=box]"])
+    graph.body.extend([', '.join(func.__name__ for func in funcs) + ' [shape=box]'])
     return graph
 
 
@@ -385,7 +385,7 @@ def dot_to_ascii(dot: str, fancy: bool = True):
     """
     import requests
 
-    url = "https://dot-to-ascii.ggerganov.com/dot-to-ascii.php"
+    url = 'https://dot-to-ascii.ggerganov.com/dot-to-ascii.php'
     boxart = 0
 
     # use nice box drawing char instead of + , | , -
@@ -394,29 +394,29 @@ def dot_to_ascii(dot: str, fancy: bool = True):
 
     stripped_dot_str = dot.strip()
     if not (
-        stripped_dot_str.startswith("graph") or stripped_dot_str.startswith("digraph")
+        stripped_dot_str.startswith('graph') or stripped_dot_str.startswith('digraph')
     ):
-        dot = "graph {\n" + dot + "\n}"
+        dot = 'graph {\n' + dot + '\n}'
 
     params = {
-        "boxart": boxart,
-        "src": dot,
+        'boxart': boxart,
+        'src': dot,
     }
 
     try:
         response = requests.get(url, params=params).text
     except requests.exceptions.ConnectionError:
-        return "ConnectionError: You need the internet to convert dot into ascii!"
+        return 'ConnectionError: You need the internet to convert dot into ascii!'
 
-    if response == "":
-        raise SyntaxError("DOT string is not formatted correctly")
+    if response == '':
+        raise SyntaxError('DOT string is not formatted correctly')
 
     return response
 
 
 def print_ascii_graph(funcs):
     digraph = funcs_to_digraph(funcs)
-    dot_str = "\n".join(map(lambda x: x[1:], digraph.body[:-1]))
+    dot_str = '\n'.join(map(lambda x: x[1:], digraph.body[:-1]))
     print(dot_to_ascii(dot_str))
 
 
@@ -442,7 +442,7 @@ def find_first_free_name(prefix, exclude_names=(), start_at=2):
     else:
         i = start_at
         while True:
-            name = f"{prefix}__{i}"
+            name = f'{prefix}__{i}'
             if name not in exclude_names:
                 return name
             i += 1
@@ -452,8 +452,8 @@ def mk_func_name(func, exclude_names=()):
     """Makes a function name that doesn't clash with the exclude_names iterable.
     Tries it's best to not be lazy, but instead extract a name from the function
     itself."""
-    name = name_of_obj(func) or "func"
-    if name == "<lambda>":
+    name = name_of_obj(func) or 'func'
+    if name == '<lambda>':
         name = lambda_name()  # make a lambda name that is a unique identifier
     return find_first_free_name(name, exclude_names)
 
@@ -468,7 +468,7 @@ def arg_names(func, func_name, exclude_names=()):
                 yield name
             else:
                 found_name = find_first_free_name(
-                    f"{func_name}__{name}", _exclude_names
+                    f'{func_name}__{name}', _exclude_names
                 )
                 yield found_name
                 _exclude_names = _exclude_names + (found_name,)
@@ -494,8 +494,8 @@ def named_partial(func, *args, __name__=None, **keywords):
 
 def _place_holder_func(*args, _sig=None, **kwargs):
     _kwargs = _sig.kwargs_from_args_and_kwargs(args, kwargs)
-    _kwargs_str = ", ".join(f"{k}={v}" for k, v in _kwargs.items())
-    return f"{_sig.name}({_kwargs_str})"
+    _kwargs_str = ', '.join(f'{k}={v}' for k, v in _kwargs.items())
+    return f'{_sig.name}({_kwargs_str})'
 
 
 def mk_place_holder_func(arg_names_or_sig, name=None, defaults=(), annotations=()):
@@ -533,7 +533,7 @@ def mk_place_holder_func(arg_names_or_sig, name=None, defaults=(), annotations=(
     sig = sig.ch_defaults(**dict(defaults))
     sig = sig.ch_annotations(**dict(annotations))
 
-    sig.name = name or sig.name or "place_holder_func"
+    sig.name = name or sig.name or 'place_holder_func'
 
     func = sig(partial(_place_holder_func, _sig=sig))
     func.__name__ = sig.name
