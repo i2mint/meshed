46ef0f9660ec32315c377b326d62fa5609d0e906
Thor Whalen <thorwhalen1@gmail.com>
2023-07-17 10:28:21 -0700

    feat: example funcs in root

---
+++ meshed/examples/__init__.py
@@ -0,0 +1,12 @@
+"""Examples of using meshed."""
+
+from meshed.examples.online_marketing import funcs as online_marketing_funcs
+from meshed.examples.vaccine_vs_no_vaccine import funcs as vaccine_vs_no_vaccine_funcs
+from meshed.examples.price_elasticity import funcs as price_elasticity_funcs
+
+
+funcs = {
+    "online_marketing": online_marketing_funcs,
+    "vaccine_vs_no_vaccine": vaccine_vs_no_vaccine_funcs,
+    "price_elasticity": price_elasticity_funcs,
+}
