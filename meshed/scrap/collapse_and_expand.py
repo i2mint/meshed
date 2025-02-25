b0a3125a577e08e3c7db049e7eed6408976d0662
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-02-25 13:49:54 +0100

    feat: add collapse and expand functions for function calls with inclusion filter

--- meshed/scrap/collapse_and_expand.py
+++ meshed/scrap/collapse_and_expand.py
@@ -5,7 +5,9 @@ https://github.com/i2mint/meshed/discussions/54
 """
 
 import re
-from typing import Union, Iterable, Optional
+from typing import Union, Iterable, Optional, Callable
+from meshed.dag import DAG
+from meshed.makers import code_to_dag
 
 
 def remove_decorator_code(
@@ -74,9 +76,29 @@ def remove_decorator_code(
     return ast.unparse(new_tree)
 
 
+def get_src_string(src: Union[str, DAG]) -> str:
+    if isinstance(src, str):
+        return src
+    elif hasattr(src, '_code_to_dag_src'):
+        return get_src_string(src._code_to_dag_src)
+    elif callable(src):
+        import inspect
+
+        return inspect.getsource(src)
+    else:
+        raise ValueError(
+            f"src should be a string or have a _code_to_dag_src (meaning the src was "
+            f"made with code_to_dag), not a {type(src)}"
+        )
+
+
 # TODO: Generalize to src that is any DAG
-def contract_function_calls(
-    src: str, call_func_name='call', *, rm_decorator='code_to_dag'
+def collapse_function_calls(
+    src: Union[str, DAG],
+    call_func_name='call',
+    *,
+    rm_decorator='code_to_dag',
+    include: Optional[Union[Iterable[str], Callable[[str], bool]]] = None,
 ):
     """
     Contract function calls in a source code string.
@@ -86,30 +108,79 @@ def contract_function_calls(
 
     Note: Doesn't work with arbitrary DAG src, only those made from code_to_dag.
     """
-    if hasattr(src, '_code_to_dag_src'):
-        import inspect
+    src_string = get_src_string(src)
 
-        dag_of_code_to_dag = src
-        src_string = inspect.getsource(dag_of_code_to_dag._code_to_dag_src)
-    elif isinstance(src, str):
-        src_string = src
-    else:
-        raise ValueError(
-            f"src should be a string or have a _code_to_dag_src (meaning the src was "
-            f"made with code_to_dag), not a {type(src)}"
-        )
+    def should_include(func_name):
+        if include is None:
+            return True
+        if isinstance(include, Iterable):
+            return func_name in include
+        if callable(include):
+            return include(func_name)
+        return False
 
-    new_src = re.sub(call_func_name + r'\(([^,]+),\s*([^)]+)\)', r'\1(\2)', src_string)
+    pattern = call_func_name + r'\(([^,]+),\s*([^)]+)\)'
+
+    def replace(match):
+        func_name, args = match.groups()
+        if should_include(func_name):
+            return f"{func_name}({args})"
+        return match.group(0)
+
+    new_src = re.sub(pattern, replace, src_string)
+
     if rm_decorator:
-        # Remove the docorator code
-        # TODO: If the decorator has arguments, we'll lose those here
         new_src = remove_decorator_code(new_src, decorator_names=rm_decorator)
 
-    if isinstance(src, str):
-        return new_src
-    else:
-        # TODO: If the decorator has arguments, this will not restore the original code
-        return code_to_dag(new_src)
+    return new_src if isinstance(src, str) else code_to_dag(new_src)
+
+
+def expand_function_calls(
+    src: Union[str, "DAG"],
+    call_func_name='call',
+    *,
+    include: Optional[Union[Iterable[str], Callable[[str], bool]]] = None,
+) -> str:
+    """
+    Inverse of collapse_function_calls.
+    It replaces calls of the form `func(arg)` with `call(func, arg)`,
+    except when the function call is part of a function definition header.
+    If include is None, it expands all function calls.
+    If include is a list of function names, only those functions are expanded.
+    If include is a callable, it's used as a filter function.
+    """
+    src_string = get_src_string(src)
+
+    def should_include(func_name):
+        if include is None:
+            return True
+        if isinstance(include, Iterable):
+            return func_name in include
+        if callable(include):
+            return include(func_name)
+        return False
+
+    pattern = r'(\b[a-zA-Z_]\w*)\(([^)]*)\)'
+
+    def replace(match):
+        # Get the start index of the match
+        index = match.start()
+        # Find the beginning of the current line
+        line_start = src_string.rfind('\n', 0, index) + 1
+        # Extract the text from the start of the line up to the match
+        current_line = src_string[line_start:index]
+        # If the current line starts with a function definition, skip expansion.
+        if re.match(r'^\s*def\s', current_line):
+            return match.group(0)
+        func_name, args = match.groups()
+        if should_include(func_name):
+            return f"{call_func_name}({func_name}, {args})"
+        return match.group(0)
+
+    new_src = re.sub(pattern, replace, src_string)
+
+    return new_src if isinstance(src, str) else code_to_dag(new_src)
+
 
 
 # ------------------------------------------------------------------------------
